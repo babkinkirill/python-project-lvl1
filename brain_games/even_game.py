cf776666d47a2eb724362a9a7f19d18ceabefab5
Kirill Babkin <babkinkirill21@gmail.com>
2022-07-19 20:22:48 +0300

    update even_game.py

--- brain_games/even_game.py
+++ brain_games/even_game.py
@@ -7,15 +7,24 @@ def run_even_game():
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}')
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    number = random.randint(1, 100)
-    print(f'Question: {number}')
-    user_answer = prompt.string(f'Your answer: ')
-    if number % 2 == 0 and user_answer == 'yes':
-        print('Correct!')
-    if number % 2 != 0 and user_answer == 'no':
-        print('Correct')
-    else:
-        print(f"Wrong answer.\nLet's try again, {name}!")
+    i = 1
+    while i <= 3:
+        number = random.randint(1, 100)
+        print(f'Question: {number}')
+        user_answer = prompt.string(f'Your answer: ')
+        if number % 2 == 0 and user_answer == 'yes':
+            print('Correct!')
+        elif number % 2 == 0 and user_answer == 'no':
+            print(f"{user_answer} is wrong answer ;(. Correct answer was 'yes'\nLet's try again,{name}!")
+            return()
+        if number % 2 != 0 and user_answer == 'no':
+            print('Correct!')
+        elif number % 2 != 0 and user_answer == 'yes':
+            print(f"{user_answer} is wrong answer ;(. Correct answer was 'no'\nLet's try again,{name}!")
+            return()
+        i += 1
+    print(f'Congratulations, {name}')
+    return()
     
 
 
