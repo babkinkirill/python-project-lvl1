0efb183412e343568ef03ee4943bafea87b67385
Kirill Babkin <babkinkirill21@gmail.com>
2022-07-22 16:26:35 +0300

    refactoring even_game.py

--- brain_games/games/even_game.py
+++ brain_games/games/even_game.py
@@ -1,29 +1,15 @@
 import prompt
 import random
+from brain_games.main_logic import run_game
 
+
+TASK = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+def get_question_and_answer():
+    question = random.randint(1, 100)
+    answer = 'yes' if question % 2 == 0 else 'no'
+    return (question, answer)
 
 def run_even_game():
-    print("Welcome to the Brain Games!")
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}')
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    i = 1
-    while i <= 3:
-        number = random.randint(1, 100)
-        print(f'Question: {number}')
-        user_answer = prompt.string('Your answer: ')
-        if number % 2 == 0 and user_answer == 'yes':
-            print('Correct!')
-        elif number % 2 == 0 and user_answer != 'yes':
-            print(f'''{user_answer} is wrong answer ;(. Correct answer was 'yes'
-Let's try again,{name}!''')
-            return()
-        if number % 2 != 0 and user_answer == 'no':
-            print('Correct!')
-        elif number % 2 != 0 and user_answer != 'no':
-            print(f'''{user_answer} is wrong answer ;(. Correct answer was 'no'
-Let's try again,{name}!''')
-            return()
-        i += 1
-    print(f'Congratulations, {name}!')
-    return()
+    run_game(get_question_and_answer, TASK)
+    return
