4310ced5cdb5c52ba993b707038335fcdfe65eff
Kirill Babkin <babkinkirill21@gmail.com>
2022-07-22 15:11:47 +0300

    update main_logic.py

--- brain_games/main_logic.py
+++ brain_games/main_logic.py
@@ -1,13 +1,14 @@
 import prompt
 
 
-def run_game():
+def run_game(get_question_and_answer, TASK):
     print("Welcome to the Brain Games!")
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}')
     print(TASK)
     i = 1
     while i <= 3:
+        question, answer, user_answer = get_question_and_answer()
         print(f'Question: {question}')
         if answer == user_answer:
             print('Correct!')
