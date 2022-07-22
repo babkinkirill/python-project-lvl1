02fbcad9659ae74d1e8b76b207eee5f52001f59b
Kirill Babkin <babkinkirill21@gmail.com>
2022-07-22 16:02:16 +0300

    refactoring calc_game.py

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -1,27 +1,20 @@
 import prompt
 import random
+from brain_games.main_logic import run_game
 
+
+TASK = 'What is the result of the expression?'
+
+def get_question_and_answer():
+    number_one = random.randint(1, 100)
+    number_two = random.randint(1, 100)
+    ops = ['+', '-', '*']
+    operator = random.choice(ops)
+    question = (f'{number_one} {operator} {number_two}')
+    answer = str(eval(str(number_one) + operator + str(number_two)))
+    return (question, answer)
 
 def run_calc_game():
-    print("Welcome to the Brain Games!")
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}')
-    print('What is the result of the expression?')
-    i = 1
-    while i <= 3:
-        number_one = random.randint(1, 100)
-        number_two = random.randint(1, 100)
-        ops = ['+', '-', '*']
-        operator = random.choice(ops)
-        result = eval(str(number_one) + operator + str(number_two))
-        print(f'Question: {number_one} {operator} {number_two}')
-        user_answer = int(prompt.string('Your answer: '))
-        if result == user_answer:
-            print('Correct!')
-        else:
-            print(f'''{user_answer} is wrong answer ;(. Correct answer was {result}
-Let's try again,{name}!''')
-            return()
-        i += 1
-    print(f'Congratulations, {name}!')
-    return()
+    run_game(get_question_and_answer, TASK)
+    return
+    
