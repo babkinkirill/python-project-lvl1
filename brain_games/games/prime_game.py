f8bd450c037e8018587a12f587c8626484c97829
Kirill Babkin <babkinkirill21@gmail.com>
2022-07-24 16:26:00 +0300

    add prime_game.py

---
+++ brain_games/games/prime_game.py
@@ -0,0 +1,15 @@
+import random
+from brain_games.main_logic import run_game
+
+
+TASK = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def get_question_and_answer():
+    
+    return(question, answer)
+
+
+def run_prime_game():
+    run_game(get_question_and_answer, TASK)
+    return
