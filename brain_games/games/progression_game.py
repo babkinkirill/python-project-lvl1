929480286e4d36410c53a65b329b8d7c26b8a989
Kirill Babkin <babkinkirill21@gmail.com>
2022-07-24 16:05:26 +0300

    refactoring progression_game.py

--- brain_games/games/progression_game.py
+++ brain_games/games/progression_game.py
@@ -1,5 +1,4 @@
 import random
-import math
 from brain_games.main_logic import run_game
 
 
@@ -7,7 +6,7 @@ TASK = 'What number is missing in the progression?'
 
 
 def get_question_and_answer():
-    progression = list(range(0, random.randint(30,66), random.randint(2,5)))
+    progression = list(range(0, random.randint(30, 66), random.randint(2, 5)))
     answer = str(random.choice(progression))
     string_progression = ' '.join(map(str, progression))
     question = string_progression.replace(answer, '..', 1)
