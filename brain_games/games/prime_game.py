15331f1034318b0bf8e0a0f4b00297d8015867d0
Kirill Babkin <babkinkirill21@gmail.com>
2022-07-24 17:57:32 +0300

    refactoring prime_game.py

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -8,8 +8,8 @@ TASK = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 def is_prime(question):
     k = 0
     for i in range(2, (question // 2) + 1):
-            if (question % i == 0):
-                 k += 1
+        if question % i == 0:
+            k = k + 1
     if k <= 0:
         return True
     else:
@@ -18,7 +18,7 @@ def is_prime(question):
 
 def get_question_and_answer():
     question = random.randint(1, 100)
-    answer = 'yes' if is_prime(question) else 'no' 
+    answer = 'yes' if is_prime(question) else 'no'
     return(question, answer)
 
 
