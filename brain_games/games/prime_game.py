8f700c77b694654f745a9c6d46a8f6c4422c0fab
Kirill Babkin <babkinkirill21@gmail.com>
2022-07-24 17:52:20 +0300

    update prime_game.py

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -5,8 +5,20 @@ from brain_games.main_logic import run_game
 TASK = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
+def is_prime(question):
+    k = 0
+    for i in range(2, (question // 2) + 1):
+            if (question % i == 0):
+                 k += 1
+    if k <= 0:
+        return True
+    else:
+        return False
+
+
 def get_question_and_answer():
-    
+    question = random.randint(1, 100)
+    answer = 'yes' if is_prime(question) else 'no' 
     return(question, answer)
 
 
