295b17805dbeffce23c1b821dbf41ed2e3ba4d60
Kirill Babkin <babkinkirill21@gmail.com>
2022-07-30 18:25:20 +0300

    brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 from brain_games.games.progression_game import get_question_and_answer
 from brain_games.games.progression_game import GAME_DESCRIPTION
-from brain_games.engine import run_game
+from brain_games import engine
 
 
 def main():
-    run_game(get_question_and_answer, GAME_DESCRIPTION)
+    engine.run_game(get_question_and_answer, GAME_DESCRIPTION)
 
 
 if __name__ == '__main__':
