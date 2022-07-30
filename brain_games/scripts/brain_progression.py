faaf6883e9beefd9b0867f799766b8c0ecc68e70
Kirill Babkin <babkinkirill21@gmail.com>
2022-07-30 17:48:29 +0300

    refactoring brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
-from brain_games.games.progression_game import run_progression_game
+from brain_games.games.progression_game import get_question_and_answer
+from brain_games.games.progression_game import GAME_DESCRIPTION
+from brain_games import engine
 
 
 def main():
-    run_progression_game()
+    engine.run_game(get_question_and_answer, GAME_DESCRIPTION)
 
 
 if __name__ == '__main__':
