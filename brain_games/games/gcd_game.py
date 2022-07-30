09568eb3d3eb713f81b5e29a233c254452faea28
Kirill Babkin <babkinkirill21@gmail.com>
2022-07-30 17:38:41 +0300

    refactoring gcd_game.py

--- brain_games/games/gcd_game.py
+++ brain_games/games/gcd_game.py
@@ -1,6 +1,5 @@
 import random
 import math
-from brain_games.engine import run_game
 
 
 GAME_DESCRIPTION = 'Find the greatest common divisor of given numbers.'
@@ -12,8 +11,3 @@ def get_question_and_answer():
     question = (f'{number_one} {number_two}')
     answer = str(math.gcd(number_one, number_two))
     return(question, answer)
-
-
-def run_gcd_game():
-    run_game(get_question_and_answer, GAME_DESCRIPTION)
-    return
