3f22700eb6d0e53b0eb3d20a41d468c6d53cb0ee
Kirill Babkin <babkinkirill21@gmail.com>
2022-07-30 17:39:19 +0300

    refactoring brain_gcd.py

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
-from brain_games.games.gcd_game import run_gcd_game
+from brain_games.games.gcd_game import get_question_and_answer
+from brain_games.games.gcd_game import GAME_DESCRIPTION
+from brain_games import engine
 
 
 def main():
-    run_gcd_game()
+    engine.run_game(get_question_and_answer, GAME_DESCRIPTION)
 
 
 if __name__ == '__main__':
