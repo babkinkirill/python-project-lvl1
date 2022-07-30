6cc0bc811099bb288257807d0675237a8a9f2500
Kirill Babkin <babkinkirill21@gmail.com>
2022-07-30 17:32:14 +0300

    refactoring even_game.py

--- brain_games/games/even_game.py
+++ brain_games/games/even_game.py
@@ -1,5 +1,4 @@
 import random
-from brain_games.engine import run_game
 
 
 GAME_DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
@@ -9,8 +8,3 @@ def get_question_and_answer():
     question = random.randint(1, 100)
     answer = 'yes' if question % 2 == 0 else 'no'
     return (question, answer)
-
-
-def run_even_game():
-    run_game(get_question_and_answer, GAME_DESCRIPTION)
-    return
