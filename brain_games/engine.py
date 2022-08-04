65ae28720faf91145d851b63043467f9d67db401
Kirill Babkin <babkinkirill21@gmail.com>
2022-08-04 15:10:28 +0300

    rafactoring engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,21 +1,22 @@
 import prompt
 
 
-def run_game(get_question_and_answer, GAME_DESCRIPTION):
+NUMBER_OF_ROUNDS = 3
+
+
+def run(get_question_and_answer, DESCRIPTION):
     print("Welcome to the Brain Games!")
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}')
-    print(GAME_DESCRIPTION)
-    number_of_rounds = 3
-    for i in range(number_of_rounds):
+    print(DESCRIPTION)
+    for i in range(NUMBER_OF_ROUNDS):
         question, answer = get_question_and_answer()
         print(f'Question: {question}')
         user_answer = input('Your answer: ')
-        if answer == user_answer:
-            print('Correct!')
-        else:
+        if answer != user_answer:
             print(f''' '{user_answer}' is wrong answer ;(. Correct answer is '{answer}'
 Let's try again, {name}!''')
-            return
-    print(f'Congratulations, {name}!')
-    return
+            break
+        print('Correct!')
+    else:
+        print(f'Congratulations, {name}!')
