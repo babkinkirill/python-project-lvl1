4e4d966110c9c7116223ad552fcd6c0fe155592c
Kirill Babkin <babkinkirill21@gmail.com>
2022-08-04 14:26:13 +0300

    rename progression_game.py to progression.py and refactoring

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,12 +1,12 @@
 import random
 
 
-GAME_DESCRIPTION = 'What number is missing in the progression?'
+DESCRIPTION = 'What number is missing in the progression?'
 
 
 def get_question_and_answer():
     progression = list(range(0, random.randint(30, 66), random.randint(2, 5)))
     answer = str(random.choice(progression))
-    string_progression = ' '.join(map(str, progression))
-    question = string_progression.replace(answer, '..', 1)
+    modified_progression = ' '.join(map(str, progression))
+    question = modified_progression.replace(answer, '..', 1)
     return(question, answer)
