2cf1f28a0c0ca35cb9a212d628fdb14d9831d0b0
Kirill Babkin <babkinkirill21@gmail.com>
2022-08-04 15:36:06 +0300

    refactoring brain_prime.py

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
-from brain_games.games import prime_game
+from brain_games.games import prime
 from brain_games import engine
 
 
 def main():
-    engine.run_game(prime_game.get_question_and_answer,
-                    prime_game.GAME_DESCRIPTION)
+    engine.run(prime.get_question_and_answer, prime.DESCRIPTION)
 
 
 if __name__ == '__main__':
