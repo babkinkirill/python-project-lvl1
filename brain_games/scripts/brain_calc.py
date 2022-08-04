cb2b760ebe8cafcb4e5cfed3901052ee33e64d4e
Kirill Babkin <babkinkirill21@gmail.com>
2022-08-04 16:01:35 +0300

    refactoring brain_calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
-from brain_games.engine import run_game
-from brain_games.games.calc_game import get_question_and_answer
-from brain_games.games.calc_game import GAME_DESCRIPTION
+from brain_games import engine
+from brain_games.games import calc
 
 
 def main():
-    run_game(get_question_and_answer, GAME_DESCRIPTION)
+    engine.run(calc.get_question_and_answer, calc.DESCRIPTION)
 
 
 if __name__ == '__main__':
