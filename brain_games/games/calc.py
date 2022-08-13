05f7a8be548f95cdbbcc5107a14c88fe99aa54ff
Kirill Babkin <babkinkirill21@gmail.com>
2022-08-13 11:44:08 +0300

    refactoring calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -12,9 +12,9 @@ def get_question_and_answer():
     math_symbol = random.choice(math_symbols)
     question = (f'{number_one} {math_symbol} {number_two}')
     if math_symbol == '+':
-        answer = str(operator.add(number_one, number_two))
+        answer = operator.add(number_one, number_two)
     elif math_symbol == '-':
-        answer = str(operator.sub(number_one, number_two))
+        answer = operator.sub(number_one, number_two)
     elif math_symbol == '*':
-        answer = str(operator.mul(number_one, number_two))
-    return (question, answer)
+        answer = operator.mul(number_one, number_two)
+    return question, str(answer)
