da76125fcc479d771d2e90d8660c63a89381ef00
Kirill Babkin <babkinkirill21@gmail.com>
2022-08-13 11:16:39 +0300

    refactoring engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -4,13 +4,13 @@ import prompt
 NUMBER_OF_ROUNDS = 3
 
 
-def run(get_question_and_answer, DESCRIPTION):
+def run(game):
     print("Welcome to the Brain Games!")
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}')
-    print(DESCRIPTION)
-    for i in range(NUMBER_OF_ROUNDS):
-        question, answer = get_question_and_answer()
+    print(game.DESCRIPTION)
+    for _ in range(NUMBER_OF_ROUNDS):
+        question, answer = game.get_question_and_answer()
         print(f'Question: {question}')
         user_answer = input('Your answer: ')
         if answer != user_answer:
